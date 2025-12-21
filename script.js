4f9d1eebfe2c357744d0d795e221dd823a1cefaf
RohitB2005 <rohitvb2005@gmail.com>
2025-12-21 17:18:52 +1300

    Added fully functional light theme with winter theme

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // --- Navbar Hamburger Logic ---
     $(".navTrigger").click(function () {
         $(this).toggleClass("active");
         console.log("Clicked menu");
@@ -7,13 +8,48 @@ $(document).ready(function() {
         $("#mainListDiv").fadeIn();
     });
 
-    // Code for the navbar background on scroll
+    // --- Sticky Navbar Logic ---
     $(window).scroll(function() {
         if ($(this).scrollTop() > 10) { 
-            // Add the .affix class to the nav
             $('.nav').addClass('affix');
         } else {
             $('.nav').removeClass('affix');
+        }
+    });
+
+    // --- Theme Toggle Logic ---
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    // Check localStorage for saved theme preference
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+        if (currentTheme === 'light-mode') {
+            $(themeToggle).removeClass('fa-moon').addClass('fa-sun');
+        }
+    }
+
+    // Toggle click event
+    $(themeToggle).on('click', function() {
+        // 1. Add rotation animation class
+        $(this).addClass('rotate-icon');
+        
+        // 2. Remove rotation class after 500ms so it can be triggered again
+        setTimeout(() => {
+            $(this).removeClass('rotate-icon');
+        }, 500);
+
+        // 3. Toggle the light-mode class on the body
+        body.classList.toggle('light-mode');
+
+        // 4. Switch Icon and Save Preference
+        if (body.classList.contains('light-mode')) {
+            $(this).removeClass('fa-moon').addClass('fa-sun');
+            localStorage.setItem('theme', 'light-mode');
+        } else {
+            $(this).removeClass('fa-sun').addClass('fa-moon');
+            localStorage.setItem('theme', 'dark-mode');
         }
     });
 
